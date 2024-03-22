6f58278c61f595a5cb46d1c528ae60f312dd404f
sanimations <smartucci157@gmail.com>
2024-03-22 17:12:02 -0400

    Finalized recipe_input/search.py files

--- exercise1.4/recipe_input.py
+++ exercise1.4/recipe_input.py
@@ -4,18 +4,16 @@ import pickle
 recipes_list = []
 all_ingredients = []
 
-#user notes how many recipes they would like to enter
-n = int(input("How many recipes would you like to enter? "))
-
-#iterates through n times for how many recipes the user wants
-for x in range(n):
-    recipe = take_recipe()
-
-    for ingredient in recipe['ingredients']:
-        if ingredient not in all_ingredients:
-            ingredients_list.append(ingredient)
-    
-    recipes_list.append(recipe)
+# Calculates difficulty
+def calc_difficulty(recipe):
+    if recipe['cooking_time']<10 and len(recipe['ingredients'])<4:
+        return "Easy"
+    elif recipe['cooking_time']<10 and len(recipe['ingredients'])>=4:
+        return "Medium"
+    elif recipe['cooking_time']>=10 and len(recipe['ingredients'])<4:
+        return "Intermediate"
+    elif recipe['cooking_time']>=10 and len(recipe['ingredients'])>=4:
+        return "Hard"
 
 # Takes recipe inputs and stores it in a dictionary, adding difficulty last
 def take_recipe():
@@ -31,19 +29,22 @@ def take_recipe():
     return recipe
 
 
-# Calculates difficulty
-def calc_difficulty(recipe):
-    if recipe['cooking_time']<10 and len(recipe['ingredients'])<4:
-        return "Easy"
-    elif recipe['cooking_time']<10 and len(recipe['ingredients'])>=4:
-        return "Medium"
-    elif recipe['cooking_time']>=10 and len(recipe['ingredients'])<4:
-        return "Intermediate"
-    elif recipe['cooking_time']>=10 and len(recipe['ingredients'])>=4:
-        return "Hard"
+#user notes how many recipes they would like to enter
+n = int(input("How many recipes would you like to enter? "))
+
+#iterates through n times for how many recipes the user wants
+for x in range(n):
+    recipe = take_recipe()
+
+    for ingredient in recipe['ingredients']:
+        if ingredient not in all_ingredients:
+            all_ingredients.append(ingredient)
+    
+    recipes_list.append(recipe)
+
 
 #User inputs filename with .bin
-fileName = input("What file are you looking for: ")
+fileName = input("Enter a filename with .bin to save recipes (if none exist, a file will be created): ")
 
 #Trying the try, except it's difficult.  Makes sense or else I would finally be done.
 #Opens file with the name given, if file not found, it will create a data dictionary
@@ -56,21 +57,22 @@ except FileNotFoundError:
         'all_ingredients' : all_ingredients
     }
     print('File Not Found! Creating new file now...')
-except:
+except Exception as e:
     data = {
         'recipes_list' : recipes_list,
         'all_ingredients' : all_ingredients
     }
-    print('An unknown error occured!')
+    print(f'An error occured: {e}')
 else:
     print('Data Successfully Loaded')
 finally:
-    for i in data:
-        print('Recipes list: ', data[recipes_list])
-        print('All Ingredients ', data[all_ingredients])
+    print('Recipes list: ', data["recipes_list"])
+    print('All Ingredients ', data["all_ingredients"])
 
 
-
-my_file = open(fileName, 'wb')
-pickle.dump(data, my_file)
-my_file.close()
+try:
+    with open(fileName, 'wb') as my_file:
+        pickle.dump(data, my_file)
+        print('Data successfully saved.')
+except Exception as e:
+    print(f'An error occurred while saving data: {e}')
--- exercise1.4/recipe_search.py
+++ exercise1.4/recipe_search.py
@@ -1,6 +1,6 @@
 import pickle
 
-def display_recipe(recipe : dict):
+def display_recipe(recipe: dict):
     print("Recipe:", recipe['name'])
     print("Cooking Time (min):", recipe['cooking_time'])
     print("Ingredients:")
@@ -8,21 +8,36 @@ def display_recipe(recipe : dict):
         print(ingredient)
     print("Difficulty:", recipe['difficulty'])
 
-def search_ingredient(data : dict):
+def search_ingredient(data: dict):
     print("Ingredients Available Across All Recipes")
-    for index, ingredient in enumerate(data['ingredients_list'], start=1):
-    print(f"{index}. {ingredient}")
+    for index, ingredient in enumerate(data['all_ingredients'], start=1):
+        print(f"{index}. {ingredient}")
 
     try:
         chosen_num = int(input("Pick a number from the list: "))
-        ingredient_searched = data['ingredients_list'][chosen_num]
-    except:
-        print('Invalid number')
+        ingredient_searched = data['all_ingredients'][chosen_num - 1]
+    except ValueError:
+        print('Invalid input: Please enter a number.')
+    except IndexError:
+        print('Invalid number: Please select a number within the range.')
+    except Exception as e:
+        print(f'An error occured: {e}')
     else:
+        found = False
         for recipe in data['recipes_list']:
             if ingredient_searched in recipe['ingredients']:
-                print(recipe['name'] + '\n')
-            else:
-                print('No recipe with that ingredient')
+                display_recipe(recipe)
+                found = True
+        if not found:
+            print('No recipe with that ingredient')
 
 
+fileName = input("Enter the name of the file containing your recipe data: ")
+
+try:
+    with open(fileName, 'rb') as my_file:
+        data = pickle.load(my_file)
+except FileNotFoundError:
+    print("File not found.")
+else:
+    search_ingredient(data)
