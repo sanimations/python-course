a3210ad4b7db13c47e60d0ddd6f8a91669d5958d
sanimations <smartucci157@gmail.com>
2024-04-17 17:33:08 -0400

    Updated bookstore app for 2.5

--- Achievement 2/exercise2.2/books_app/src/salespersons/models.py
+++ Achievement 2/exercise2.2/books_app/src/salespersons/models.py
@@ -6,6 +6,7 @@ class Salesperson(models.Model):
     username =models.OneToOneField(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=120)
     bio=models.TextField(default="nobio...")
+    pic = models.ImageField(upload_to='salespersons', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
