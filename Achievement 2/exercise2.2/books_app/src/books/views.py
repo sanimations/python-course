88041a4869c9b401c25dbd7fd49b7bd7ba5db09a
sanimations <smartucci157@gmail.com>
2024-04-23 18:23:26 -0400

    Updated journal and bookstore task

--- Achievement 2/exercise2.2/books_app/src/books/views.py
+++ Achievement 2/exercise2.2/books_app/src/books/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView   #to display lists
 from .models import Book                #to access Book model
+from django.contrib.auth.mixins import LoginRequiredMixin   #to protect class-based view
 
-class BookListView(ListView):            #class-based view
+class BookListView(LoginRequiredMixin, ListView):            #class-based view
     model = Book                         #specify model
     template_name = 'books/main.html'    #specify template 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/detail.html'
